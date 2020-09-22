91c210928fe530d0152cbe2a17b880871234bc71
HsuHsiaoHsuan <hsuhsiaohsuan@gmail.com>
2020-09-22 21:16:47 +0800

    part 6 dagger-hilt

---
+++ app/src/main/java/idv/hsu/keeprunning/BaseApplication.kt
@@ -0,0 +1,9 @@
+package idv.hsu.keeprunning
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
--- app/src/main/java/idv/hsu/keeprunning/MainActivity.kt
+++ app/src/main/java/idv/hsu/keeprunning/MainActivity.kt
@@ -2,10 +2,20 @@ package idv.hsu.keeprunning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import idv.hsu.keeprunning.db.RunDAO
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
